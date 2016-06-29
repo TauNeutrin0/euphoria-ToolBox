07a75f9d0454a4dee1456b015b3764c6337f6370
TauNeutrin0 <tauneutrin00@gmail.com>
2016-06-29 21:42:07 +0000

    Initial commit

---
+++ src/main/java/StructuredBot.java
@@ -0,0 +1,13 @@
+import euphoria.Bot;
+
+
+public class StructuredBot extends Bot{
+    
+    public StructuredBot() {
+        
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
